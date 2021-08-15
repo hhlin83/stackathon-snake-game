c82921074acea2d6a2356cbed166541e8107237b
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-14 23:18:20 -0700

    Refactor a bit of follower component

--- src/components/Follower.js
+++ src/components/Follower.js
@@ -1,21 +1,17 @@
-import React, { useRef, useEffect, useContext } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { GameContext } from './GameManager';
 
 export default function Follower({ position }) {
   const follower = useRef();
-  const history = [];
-  let collected = false;
-  let reachLast = false;
-  let startTrack = false;
-  let followerIdx = null;
   const { snake } = useContext(GameContext);
+  const targetHistory = [];
+  let collected = false;
+  let reachedLast = false;
+  let startFollow = false;
+  let snakeIdx = null;
 
-  useEffect(() => {
-    console.log(snake);
-  }, []);
-
-  useFrame((state, delta) => {
+  useFrame(() => {
     const followerPos = follower.current.position;
     if (!collected && snake[0]) {
       const playerPos = snake[0].current.position.clone();
@@ -23,32 +19,32 @@ export default function Follower({ position }) {
       if (distance < 0.5) {
         collected = true;
         snake.push(follower);
-        followerIdx = snake.length - 1;
+        snakeIdx = snake.length - 1;
         console.log('collected!', snake);
       }
     } else {
-      const target = snake[followerIdx - 1];
+      const target = snake[snakeIdx - 1];
       const targetPos = target.current.position.clone();
 
-      if (!reachLast) {
+      if (!reachedLast) {
         const distance = followerPos.distanceTo(targetPos);
         if (distance < 0.5) {
-          reachLast = true;
+          reachedLast = true;
           console.log('reach last!');
         }
       } else {
-        history.push(targetPos);
-        if (!startTrack) {
+        targetHistory.push(targetPos);
+        if (!startFollow) {
           follower.current.lookAt(targetPos);
           const distance = followerPos.distanceTo(targetPos);
           if (distance > 1) {
-            startTrack = true;
+            startFollow = true;
             console.log('start tracking!');
           }
         } else {
-          const historyPos = history.shift();
+          const historyPos = targetHistory.shift();
           follower.current.lookAt(historyPos);
-          follower.current.position.copy(historyPos);
+          followerPos.copy(historyPos);
         }
       }
     }
